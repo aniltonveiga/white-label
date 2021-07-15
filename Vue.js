f1b27af5d6f9ec761a1efbac01e81d518d5b4176
Anilton Veiga <anilton.veigaa@gmail.com>
2021-07-15 14:56:52 -0300

    Update Vue.js

--- Vue.js
+++ Vue.js
@@ -26,10 +26,10 @@ Vue.use(VueCryptojs)
 Vue.use(Encrypts)
 
 // PROD ou DEV
-Vue.prototype.API_ENDPOINT = process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://api.99jobs.com"
-Vue.prototype.DELTA_ENDPOINT = process.env.NODE_ENV == "development" ? "http://localhost:3001" : "https://99jobs.com/"
-Vue.prototype.PLATFORM_TEST = process.env.NODE_ENV == "development" ? "http://localhost:3002" : "https://api-platform-test.99jobs.com"
-Vue.prototype.OPPORTUNITIES_ENDPOINT = process.env.NODE_ENV == "development" ? "http://localhost:3003" : "https://api-oportunidades.99jobs.com/"
+//Vue.prototype.API_ENDPOINT = process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://api.99jobs.com"
+//Vue.prototype.DELTA_ENDPOINT = process.env.NODE_ENV == "development" ? "http://localhost:3001" : "https://99jobs.com/"
+//Vue.prototype.PLATFORM_TEST = process.env.NODE_ENV == "development" ? "http://localhost:3002" : "https://api-platform-test.99jobs.com"
+//Vue.prototype.OPPORTUNITIES_ENDPOINT = process.env.NODE_ENV == "development" ? "http://localhost:3003" : "https://api-oportunidades.99jobs.com/"
 
 Vue.prototype.API_ENDPOINT = "https://api.99jobs.com"
 Vue.prototype.DELTA_ENDPOINT = "https://99jobs.com/"
